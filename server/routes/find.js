664f0d32e34c5cc15c9d23b689cce2565aa9d6f3
Jenica Rawtani <jenicarawtani@jenicarawtani-macbookpro.roam.corp.google.com>
2018-10-17 16:25:31 +0100

    move function

--- server/routes/find.js
+++ server/routes/find.js
@@ -1,5 +1,29 @@
 module.exports =  function(req, res, next, file) {
 
+	
+	async function uploadFile(bucketName, filename) {
+
+		// Imports the Google Cloud client library
+		const {Storage} = require('@google-cloud/storage');
+
+		// Creates a client
+		const storage = new Storage();
+
+		// Uploads a local file to the bucket
+		await storage.bucket(bucketName).upload(filename, {
+		  // Support for HTTP requests made with `Accept-Encoding: gzip`
+		  gzip: true,
+		  metadata: {
+		    // Enable long-lived HTTP caching headers
+		    // Use only if the contents of the file will never change
+		    // (If the contents will change, use cacheControl: 'no-cache')
+		    cacheControl: 'public, max-age=31536000',
+		  },
+		});
+
+		console.log(`${filename} uploaded to ${bucketName}.`);
+	};
+
 	/**
 	 * TODO(developer): Uncomment the following lines before running the sample.
 	 */
@@ -33,27 +57,4 @@ module.exports =  function(req, res, next, file) {
 	  });
 
     // res.send('topresult');
-};
-
-async function uploadFile(bucketName, filename) {
-
-	// Imports the Google Cloud client library
-	const {Storage} = require('@google-cloud/storage');
-
-	// Creates a client
-	const storage = new Storage();
-
-	// Uploads a local file to the bucket
-	await storage.bucket(bucketName).upload(filename, {
-	  // Support for HTTP requests made with `Accept-Encoding: gzip`
-	  gzip: true,
-	  metadata: {
-	    // Enable long-lived HTTP caching headers
-	    // Use only if the contents of the file will never change
-	    // (If the contents will change, use cacheControl: 'no-cache')
-	    cacheControl: 'public, max-age=31536000',
-	  },
-	});
-
-	console.log(`${filename} uploaded to ${bucketName}.`);
 };
