8e534339f0dd68507899ae684cc036dab156569c
Jenica Rawtani <jenicarawtani@jenicarawtani-macbookpro.roam.corp.google.com>
2018-10-17 15:28:51 +0100

    vision api

--- server/routes/find.js
+++ server/routes/find.js
@@ -1,22 +1,25 @@
 module.exports =  function(req, res, next) {
-  res.send("find route");
+
+
+	// Imports the Google Cloud client library
+	const vision = require('@google-cloud/vision');
+
+	// Creates a client
+	const client = new vision.ImageAnnotatorClient();
+
+	// Performs label detection on the image file
+	client
+	  .labelDetection('gs://bucket-name-123/demo-image.jpg')
+	  .then(results => {
+	    const labels = results[0].labelAnnotations;
+
+	    console.log('Labels:');
+	    labels.forEach(label => console.log(label.description));
+	    const topresult = labels[0].description;
+	  })
+	  .catch(err => {
+	    console.error('ERROR:', err);
+	  });
+
+    res.send(topresult);
 };
-
-// Imports the Google Cloud client library
-const vision = require('@google-cloud/vision');
-
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
-
-// Performs label detection on the image file
-client
-  .labelDetection('gs://bucket-name-123/demo-image.jpg')
-  .then(results => {
-    const labels = results[0].labelAnnotations;
-
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
