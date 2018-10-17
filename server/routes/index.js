f61139a484d4b85696fd1e615f042d6bbb4c4ad1
AstroAsh <astroaisha@gmail.com>
2018-10-17 13:42:29 +0100

    🏗 setup server architecture

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
-
+const router = require('express').Router();
+const find = require('./find')
+const products = require('./products')
+const productsId = require('./products_id')
+const users = require('./users')
+const usersId = require('./users_id')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("Hello!!");
-});
+router.use('/api/find', find);
+router.use('/api/products', products);
+router.use('/api/products/{id}', productsId);
+router.use('/api/users', users);
+router.use('/api/users/{id}', usersId);
 
 module.exports = router;
