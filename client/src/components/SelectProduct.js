9e626e2d3be3ee21212fec39781c4d15ee38b103
Clem Capel-Bird <clemcapelbird@gmail.com>
2018-10-17 15:38:14 +0100

    Products page working with a json document

--- client/src/components/SelectProduct.js
+++ client/src/components/SelectProduct.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import products from '../mockProducts.json';
 
-const SelectProduct = () => (
-<div className="card">
-  <div className="card-body">
-    <h5 className="card-title">Select Product</h5>
-    <p className="card-text">Pick the plastic product you have used recently to log and check the environmental impact.</p>
-    <a href="/facts" className="btn btn-primary">Water bottle</a>
-  </div>
-</div>
-);
+const getAllProducts = () => {
+  return products;
+}
+
+const SelectProduct = () => {
+  const products = getAllProducts();
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Select Product</h5>
+        <p className="card-text">Pick the plastic product you have used recently to log and check the environmental impact.</p>
+        {products.map(product =>
+          <a 
+            href={`/facts/${product.productId}`}
+            className="btn btn-primary"
+            >
+            {product.productName}
+          </a>
+        )} 
+      </div>
+    </div>
+  )
+};
 
 export default SelectProduct;
 
