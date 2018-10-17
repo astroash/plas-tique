64729386e211eba098caa6f024021684950e3f7e
Clem Capel-Bird <clemcapelbird@gmail.com>
2018-10-17 18:37:25 +0100

    Facts and Alternatives call api

--- client/src/components/Alternatives.js
+++ client/src/components/Alternatives.js
@@ -1,23 +1,40 @@
 import React from 'react';
-
-const Alternatives = (props) => {
-  const { location: { state: { product }}} = props;
+export default class Alternatives extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {product: ''};
+    }
+  
+    componentDidMount() {
+      const { params: { id } } = this.props.match;
+      this.setState({productId: id})
+  
+      fetch(`/api/products/${id}`)
+      .then((res) => 
+          res.json())
+          .then(( result ) => 
+          this.setState({ product: result }));
+          console.log(this.state.product)
+    }
+  
+    render() {
 
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Alternatives to {product.productName}s</h5>
-        {product.recommendations.map((recommendation, index) => 
+        <h5 className="card-title">Alternatives to {this.state.product.productName}s</h5>
+        {
+          this.state.product ? this.state.product.recommendations.map((recommendation, index) => 
           <p key={index}
             className="card-text"
           >
             <b>{recommendation}</b>
           </p>
-        )}
-        <img src={product.alternativeProduct} alt="alternative-pic" />
+         ) : ''
+        }
+        <img src={this.state.product.alternativeProduct} alt="alternative-pic" />
         <a href="/" className="btn btn-primary">Back to Home</a>
       </div>
     </div>
-)};
-
-export default Alternatives;
+  )}
+}
--- client/src/components/Facts.js
+++ client/src/components/Facts.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import products from '../mockProductFacts.json';
 
-const getProduct = (id) => {
-  return products[id];
-}
+export default class Facts extends React.Component {
+  constructor(props) {
+    super(props);
+    // const { params: { id } } = this.props.match;
+    this.state = {product: ''};
+  }
 
-const Facts = ({ match }) => {
-  const { params: { id } } = match;
-  const product = getProduct(id);
+  componentDidMount() {
+    const { params: { id } } = this.props.match;
+    this.setState({productId: id})
+
+    fetch(`/api/products/${id}`)
+    .then((res) => 
+        res.json())
+        .then(( result ) => 
+        this.setState({ product: result }));
+        console.log(this.state.product)
+  }
+
+  render() {
 
   return (
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title">Facts about {product.productName}s</h5>
-        <p className="card-text"><b>{product.weight}g plastic</b></p>
-        <p className="card-text">Takes <b>{product.lifeSpan} years</b> to biodegrade</p>
-        <Link to={{ pathname: `/alternatives/${id}`, state: { product }}} className="btn btn-primary">
+        <h5 className="card-title">Facts about {this.state.product.productName}s</h5>
+        <p className="card-text"><b>{this.state.product.weight}g plastic</b></p>
+        <p className="card-text">Takes <b>{this.state.product.lifeSpan} years</b> to biodegrade</p>
+        <Link to={`/alternatives/${this.state.productId}`} className="btn btn-primary">
           Alternatives
         </Link>
       </div>
   </div>
-)};
-
-export default Facts;
+  )
+  }
+}
